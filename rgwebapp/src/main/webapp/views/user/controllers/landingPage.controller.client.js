3563504abdbb57e395ddb5733270e13baf2e7856
sumitb1606 <sumitbhanwala@Sumits-MacBook-Pro.local>
2017-11-30 20:36:26 -0500

    Removed un necessary  controller

--- rgwebapp/src/main/webapp/views/user/controllers/landingPage.controller.client.js
+++ rgwebapp/src/main/webapp/views/user/controllers/landingPage.controller.client.js
@@ -16,10 +16,18 @@
             var promise = UserService.createUser(vm.user);
             console.log(promise);
             promise.then(function (response) {
-                console.log(response);
+                closeModal();
+                $timeout(function () {
+                        $location.url("/user/profilePage/:UID");
+                }, 350);
             },function (error) {
                 console.log(error);
             })
+        }
+
+        // Function for closing the modal
+        function closeModal() {
+            $('.modal').modal('hide');
         }
     }
 })();
