5fdeace2d60e1cb9400b804260988be06683f4b0
sumitb1606 <sumitbhanwala@Sumits-MacBook-Pro.local>
2017-11-30 19:51:52 -0500

    Added the controller

---
+++ rgwebapp/src/main/webapp/views/user/controllers/profilePage.controller.client.js
@@ -0,0 +1,11 @@
+(function() {
+
+    angular
+        .module("RoomGuardWebApp")
+        .controller("ProfilePageController",ProfilePageController);
+
+    function ProfilePageController ($location, $routeParams, $timeout, UserService) {
+        var vm = this;
+    }
+
+})();
